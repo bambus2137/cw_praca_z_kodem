48033685ec5796b81cc416a60dafa4226bde8cfb
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-03-24 17:12:00 +0100

    trzy

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ install:
 	pip install -r .\requirements.txt	
 run:
 	python.exe -m flask run
+lint:
+	python.exe -m pylint .\app.py
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 from markupsafe import escape
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route("/hello/")
+@app.route("/hello/<name>")
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template("hello.html", name="anton")
